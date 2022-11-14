53696e9b92844c72b3fbfcf61e6dd4b3498de293
rebaz36 <rebaz415@gmail.com>
2022-11-15 00:16:48 +0300

    Add NavBar Component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import PlanetLogo from '../Images/planet.png';
 import './NavBar.css';
 
 function NavBar() {
   return (
     <div className="NavBar">
-      <div className="userOval">
+      <div className="Logo-Container">
         <img className="Logo" src={PlanetLogo} alt="" />
+        <h1>Traverlers Hub</h1>
       </div>
-      <div className="Nav">
-        <h1>Empty Nav</h1>
-        <h1>Empty Nav</h1>
+      <div className="Nav-List">
+        <NavLink className="Nav-Item" to="/">
+          Rockets
+        </NavLink>
+        <NavLink className="Nav-Item" to="/missions">
+          Missions
+        </NavLink>
+        <h1>|</h1>
+        <NavLink className="Nav-Item" to="/profile">
+          My Profile
+        </NavLink>
       </div>
     </div>
   );
