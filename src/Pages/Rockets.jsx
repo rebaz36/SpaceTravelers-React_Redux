3d6c00cec3a73a026f9bf60129c5d79a2db7b541
ndorrh <ndorrh@gmail.com>
2022-11-15 13:54:42 +0100

    Dispatch getRockets AsyncThunk in Rocket page to display it by passing it as prop to RocketCard component

--- src/Pages/Rockets.jsx
+++ src/Pages/Rockets.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import RocketCard from '../Components/Rocket/RocketCard';
+import { getRockets } from '../Redux/Rockets/Rockets';
 
 export default function Rockets() {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.Rockets.rockets);
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+  console.log(rockets);
   return (
     <div>
       <RocketCard />
