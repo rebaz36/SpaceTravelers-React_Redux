1cb118b704b24cff7dfc888864a7a0cfe7b4b5dd
ndorrh <ndorrh@gmail.com>
2022-11-15 12:19:56 +0100

    Add RocketCard Component to diplay rockets and info

--- src/Components/Rocket/RocketCard.js
+++ src/Components/Rocket/RocketCard.js
@@ -4,7 +4,16 @@ import './RocketCard.css';
 function RocketCard() {
   return (
     <div className="Rocket__card">
-      <h1>I am starting</h1>
+      <img src="https://i.ytimg.com/vi/1yBwWLunlOM/maxresdefault.jpg" alt="Rocket" />
+      <div className="Rocket__info">
+        <h3>Falcon 1</h3>
+        <p>
+          The Rockets section displays a list of all available SpaceX
+          rockets. Users can book each rocket by clicking the reservation button or
+          cancel the previously made booking
+        </p>
+        <button type="button">Reserve Rocket</button>
+      </div>
     </div>
   );
 }
