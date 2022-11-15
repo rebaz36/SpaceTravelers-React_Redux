f27deaed491c5398b8eb2a164c518d62eaa8e590
ndorrh <ndorrh@gmail.com>
2022-11-15 12:25:01 +0100

    Add basic styling for RocketCard Component

--- src/Components/Rocket/RocketCard.js
+++ src/Components/Rocket/RocketCard.js
@@ -8,6 +8,7 @@ function RocketCard() {
       <div className="Rocket__info">
         <h3>Falcon 1</h3>
         <p>
+          <span>reserved</span>
           The Rockets section displays a list of all available SpaceX
           rockets. Users can book each rocket by clicking the reservation button or
           cancel the previously made booking
